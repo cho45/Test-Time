7295f18bdcf1bc33e7a6e4a76614c6931505ffbc
cho45 <cho45@lowreal.net>
2011-06-21 10:44:29 +0900

    init

---
+++ lib/Test/Time.pm
@@ -0,0 +1,38 @@
+package Test::Time;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Test::Time - 
+
+=head1 SYNOPSIS
+
+  use Test::Time;
+
+
+=head1 DESCRIPTION
+
+Test::Time is 
+
+=head1 AUTHOR
+
+cho45 E<lt>cho45@lowreal.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
